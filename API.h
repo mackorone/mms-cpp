1270fabebc6c32ee27a37edb6f594e9969b0b284
Mack Ward <mackorone@gmail.com>
2019-02-03 14:06:57 -0500

    Add API and Main

---
+++ API.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+class API {
+
+public:
+
+    static int mazeWidth();
+    static int mazeHeight();
+
+    static bool wallFront();
+    static bool wallRight();
+    static bool wallLeft();
+
+    static void moveForward();
+    static void turnRight();
+    static void turnLeft();
+
+    static void setWall(int x, int y, char direction);
+    static void clearWall(int x, int y, char direction);
+
+    static void setColor(int x, int y, char color);
+    static void clearColor(int x, int y);
+    static void clearAllColor();
+
+    static void setText(int x, int y, const std::string& text);
+    static void clearText(int x, int y);
+    static void clearAllText();
+
+    static bool wasReset();
+    static void ackReset();
+
+};
---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include "API.h"
+
+int main(int argc, char* argv[]) {
+    while (true) {
+        if (!API::wallLeft()) {
+            API::turnLeft();
+        }
+        while (API::wallFront()) {
+            API::turnRight();
+        }
+        API::moveForward();
+    }
+}
